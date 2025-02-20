4b91ce8d0e70a75ed5ba93b2d5cf45f8a6f665a0
Andres Guerrero <166781613+MiguelAndresGuerrero@users.noreply.github.com>
2025-02-20 15:53:35 -0500

    Delete Ejercicios Java directory

--- Ejercicios Java/Ejercicio12.java
+++
@@ -1,8 +0,0 @@
-public class Ejercicio12 {
-    public static void main(String[] args) {
-        
-        for (int i = 0; i <= 255; i++) {
-            System.out.println("ASCII " + i + " = " + (char) i);
-        }
-    };
-};
--- Ejercicios Java/Ejercicio6.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio6 {
-    public static void main(String[] args) {
-        for (char i = 'z'; i >= 'a'; i--) {
-            System.out.println(i);
-        };
-    };
-};
